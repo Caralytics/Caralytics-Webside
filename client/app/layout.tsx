003a607130983fd8916bbf6ad0be80d3d7e5dddb
Caralytics <office@caralytics.de>
2026-01-05 11:26:03 +0100

    Update layout.tsx

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -13,7 +13,7 @@ const manrope = Manrope({
 
 export const metadata: Metadata = {
   title:
-    "Kfz-Gutachter Köln – Unfallgutachten & Schadengutachten | Caralytics – Engineering",
+    "Kfz-Gutachter Köln – Unfallgutachten & Schadengutachten | Caralytics Engineering",
   description:
     "Unfall in Köln passiert? Unfallgutachten durch Fahrzeugingenieur und TÜV-geschulten Kfz-Sachverständigen in Köln – wir sichern Beweise, ermitteln die Schadenhöhe und unterstützen bei der Regulierung mit der Versicherung.",
   icons: "/assets/logo/favicon-32x32-and-16x16/32x32.png",
