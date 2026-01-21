5ca223170d85e82c49149d1471f817ba8ec50cea
Caralytics <office@caralytics.de>
2026-01-21 10:49:07 +0100

    Update robots.ts

--- client/app/robots.ts
+++ client/app/robots.ts
@@ -1,5 +1,11 @@
-User-agent: *
-Allow: /
-Disallow: /api/
-Disallow: /_next/
-Disallow: /assets/
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/_next/', '/assets/'],
+    },
+  }
+}
